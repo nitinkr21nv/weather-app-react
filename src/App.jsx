12eb17a8699608bb1b68cd4cf87e9a10c5f7ff56
nitinkr21nv <nitin2111kr@gmail.com>
2026-07-05 13:22:10 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import WeatherApp from "./WeatherApp";
+export default function App() {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+}
---
+++ src/WeatherApp.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+export default function WeatherApp() {
+  const [weatherInfo, setWeatherInfo] = useState({
+    city: "Wonderland",
+    feelsLike: 24.84,
+    temp: 25.05,
+    tempMin: 25.05,
+    tempMax: 25.05,
+    humidity: 47,
+    weather: "haze",
+  });
+
+  let updateInfo = (newInfo) => {
+    setWeatherInfo(newInfo);
+  };
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Weather App by Delta</h1>
+      <SearchBox updateInfo={updateInfo} />
+      <InfoBox info={weatherInfo} />
+    </div>
+  );
+}
